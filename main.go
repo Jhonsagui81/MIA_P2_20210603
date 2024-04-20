f4708c0f2514dc86085b9ab8ee02b9e341411ff1
Jhonsagui81 <reyesjhonatan86@gmail.com>
2024-04-19 20:45:07 -0600

    intento coneccion

---
+++ main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+func main() {
+	router := mux.NewRouter().StrictSlash(true)
+
+	// Endpoint para recibir el nombre
+	router.HandleFunc("/comand", InputCommand)
+
+	fmt.Println("Servidor iniciado en el puerto 3000")
+	http.ListenAndServe(":3000", router)
+}
+
+func InputCommand(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Si llego el comando")
+}
